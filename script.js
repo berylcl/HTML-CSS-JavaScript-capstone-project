962168fbea97bc6ffc0b55841e3ce9738b9ed181
berylcl <gakiiberyl@gmail.com>
2023-04-20 11:12:57 +0100

    Adding appendChild() or innerHTML() to meet requirements

--- script.js
+++ script.js
@@ -54,37 +54,70 @@ const speakers = [
     img: 'images/ahmad alkurdy.png',
   },
 ];
-
 const referencePosition = document.getElementById('main-program');
 
 const createspeakerStructureHTML = () => {
-  referencePosition.insertAdjacentHTML('afterend',
-    `<section class="speakers" id="featured-speakers">
-     <div class="section-title"><h2>Featured Speakers</h2></div>
-     <span class="red-under"></span>
-     <ul class="speakers-container"></ul></section>`);
+  const speakersSection = document.createElement('section');
+  speakersSection.classList.add('speakers');
+  speakersSection.setAttribute('id', 'featured-speakers');
+
+  const sectionTitle = document.createElement('div');
+  sectionTitle.classList.add('section-title');
+  const titleHeading = document.createElement('h2');
+  titleHeading.textContent = 'Featured Speakers';
+  sectionTitle.appendChild(titleHeading);
+
+  const redUnderline = document.createElement('span');
+  redUnderline.classList.add('red-under');
+
+  const speakersList = document.createElement('ul');
+  speakersList.classList.add('speakers-container');
+
+  speakersSection.appendChild(sectionTitle);
+  speakersSection.appendChild(redUnderline);
+  speakersSection.appendChild(speakersList);
+
+  referencePosition.parentNode.insertBefore(speakersSection, referencePosition.nextSibling);
 };
 
 const createspeakerCard = () => {
+  const speakersGrid = document.querySelector('.speakers-container');
   speakers.forEach((speaker) => {
-    const speakersGrid = document.querySelector('.speakers-container');
+    const speakerCard = document.createElement('li');
+    speakerCard.classList.add('card');
+    speakerCard.setAttribute('id', 'speakers');
 
-    speakersGrid.insertAdjacentHTML('beforeend',
-      `<li class="card" id = "speakers">
-         <div class="speaker-image">
-         <img src="${speaker.img}" alt="speaker photo"></div>
-        <div class="speaker__dsc">
-        <h3>${speaker.name}</h3>
-        <p class="dsc-1">${speaker.profession}</p>
-        <span></span>
-        <p class="dsc-2">${speaker.description}</p>
-        </div>
-        </li>`);
+    const speakerImage = document.createElement('div');
+    speakerImage.classList.add('speaker-image');
+    const image = document.createElement('img');
+    image.setAttribute('src', speaker.img);
+    image.setAttribute('alt', 'speaker photo');
+    speakerImage.appendChild(image);
+
+    const speakerDescription = document.createElement('div');
+    speakerDescription.classList.add('speaker__dsc');
+    const nameHeading = document.createElement('h3');
+    nameHeading.textContent = speaker.name;
+    const professionPara = document.createElement('p');
+    professionPara.classList.add('dsc-1');
+    professionPara.textContent = speaker.profession;
+    const descriptionPara = document.createElement('p');
+    descriptionPara.classList.add('dsc-2');
+    descriptionPara.textContent = speaker.description;
+
+    speakerDescription.appendChild(nameHeading);
+    speakerDescription.appendChild(professionPara);
+    speakerDescription.appendChild(document.createElement('span'));
+    speakerDescription.appendChild(descriptionPara);
+
+    speakerCard.appendChild(speakerImage);
+    speakerCard.appendChild(speakerDescription);
+
+    speakersGrid.appendChild(speakerCard);
   });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   createspeakerStructureHTML();
-
   createspeakerCard();
 });
